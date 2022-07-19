7e0e52e01d50fd03900936dadbad3fdcab03cbf3
Solomon Xie <solomonxiewise@gmail.com>
2022-07-20 01:46:06 +0800

    hello world

---
+++ learn-x-in-y-minutes/01-helloworld.c
@@ -0,0 +1,9 @@
+/* Hello, world! */
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("hello, world!");
+    return 0;
+}
---
+++ learn-x-in-y-minutes/02-data-type.c
@@ -0,0 +1,20 @@
+/* REF: https://learnxinyminutes.com/docs/c/ */
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+    printf("int: %d\n", 123);
+    printf("float: %f\n", 123.456);
+
+    int x_int = 123;
+    printf("int: %d\n", x_int);
+
+    float x_float = 123.456;
+    printf("float: %f\n", x_float);
+
+    short x_short = 123;
+    printf("short: %d", x_short);
+    return 0;
+}
